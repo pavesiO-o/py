a7b9bd3d22ccd943057c0a3438dc343c6dd7178b
pavesi <gabrielpavesicorrea@gmail.com>
2026-05-25 15:13:45 -0300

    calculadora criada e instanciada

---
+++ hehels/bgl do git/calculadora.py
@@ -0,0 +1,14 @@
+def  soma(a, b):
+    return print(a + b)
+
+soma(4,)
+
+def subtracao(a, b):
+    return print(a - b)
+
+def multiplicacao(a, b):
+    return print(a * b)
+
+def divisao(a, b):
+    return print(a / b)
+
---
+++ hehels/bgl do git/gitfodasi.py
@@ -0,0 +1,5 @@
+import calculadora
+
+c = calculadora
+
+c.soma(6,7)
